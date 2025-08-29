fe4f45e4a7a7c79d0b9412ff0f2bbfe62e6373b7
Alex Badovski <riscador@gmail.com>
2025-08-29 15:16:45 +0300

    first task

---
+++ task1.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+def total_salary(path: str) -> tuple[str, str] | None:
+    p = Path(path)
+    if not p.exists():
+        print('Path does not exist')
+        return None
+
+    if p.is_dir():
+        print('Path is not a file')
+        return None
+
+    total = 0;
+    lines = 0;
+    with open(path, 'r') as fh:
+        while True:
+            line = fh.readline()
+            if not line:
+                break
+
+            raw = line.split(',')[1]
+            try:
+                salary = float(raw)
+                total += salary
+                lines += 1
+            except ValueError:
+                salary = 0
+
+    fh.close()
+
+    average = total / lines
+    return (f"{total:.2f}", f"{average:.2f}") if lines else None
+
+print(total_salary('salary.txt'))
